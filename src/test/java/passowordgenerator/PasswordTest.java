cd954db8b279b4c2a31b3f68ced7394d249e3b82
Sushrut Ghimire <sushrutghimire1@gmail.com>
2021-03-29 18:39:48 +0530

    updated

--- src/test/java/passowordgenerator/PasswordTest.java
+++ src/test/java/passowordgenerator/PasswordTest.java
@@ -1,14 +1,20 @@
 package passowordgenerator;
 
 
+import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
 public class PasswordTest {
+
+    @Rule
+    public RepeatRule repeatRule = new RepeatRule();
+
     private final PasswordGenerator passwordGenerator = new PasswordGeneratorImpl();
 
     @Test
+    @RepeatRule.Repeat(times = 10)
     public void shouldThrowErrorWhenNotValid() {
         String random = passwordGenerator.generatePassword();
         System.out.println(random);
