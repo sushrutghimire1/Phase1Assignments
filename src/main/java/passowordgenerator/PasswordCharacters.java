b097b599c0d440e87996bdc110f445f5f6ed080f
Sushrut Ghimire <sushrut.ghimire@clusus.com>
2021-04-02 01:37:17 +0530

    added password impl

--- src/main/java/passowordgenerator/PasswordCharacters.java
+++ src/main/java/passowordgenerator/PasswordCharacters.java
@@ -3,12 +3,20 @@ package passowordgenerator;
 public enum PasswordCharacters {
     ALPHABETS("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
     NUMBERS("1234567890"),
-    SYMBOLS("_$#%");
+    SYMBOLS(PasswordCharacters.getSymbols());
 
     private final String characters;
 
     PasswordCharacters(String characters) {
         this.characters = characters;
+    }
+
+    public static String getSymbols() {
+        StringBuilder symbols = new StringBuilder();
+        for (Symbols symbol : Symbols.values()) {
+            symbols.append(symbol.getSymbol());
+        }
+        return symbols.toString();
     }
 
     public String getCharacters() {
